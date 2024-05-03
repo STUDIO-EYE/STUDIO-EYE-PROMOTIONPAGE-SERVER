f37a674ec9c67bf6f168487c593e322f8a7aef4e
MoonInbae <mib010213@gmail.com>
2024-05-03 17:13:13 +0900

    [Feat] 회사 정보 기능 구현
    - controller에 UD api 추가

--- src/main/java/com/example/promotionpage/domain/companyInformation/api/CompanyInformationController.java
+++ src/main/java/com/example/promotionpage/domain/companyInformation/api/CompanyInformationController.java
@@ -1,8 +1,7 @@
 package com.example.promotionpage.domain.companyInformation.api;
 
 import com.example.promotionpage.domain.companyInformation.application.CompanyInformationService;
-import com.example.promotionpage.domain.companyInformation.dto.request.CreateCompanyInformationRequestDto;
-import com.example.promotionpage.domain.request.dto.request.CreateRequestDto;
+import com.example.promotionpage.domain.companyInformation.dto.request.*;
 import com.example.promotionpage.global.common.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
 
 @Tag(name = "회사 정보(기본 정보, 소개 정보, 상세 정보) API", description = "회사 정보(기본 정보, 소개 정보, 상세 정보) 등록 / 수정 / 삭제 / 조회")
 @RestController
@@ -58,5 +56,68 @@ public class CompanyInformationController {
     @GetMapping("/company/detail")
     public ApiResponse retrieveCompanyDetailInformation() {
         return companyInformationService.retrieveCompanyDetailInformation();
+    }
+
+    @Operation(summary = "회사 전체 정보 수정 API")
+    @PutMapping("/company/information")
+    public ApiResponse updateAllCompanyInformation(@Valid @RequestPart("request") UpdateAllCompanyInformationRequestDto dto,
+                                                   @RequestPart(value = "logoImageUrl", required = false) MultipartFile logoImageUrl,
+                                                   @RequestPart(value = "sloganImageUrl", required = false) MultipartFile sloganImageUrl) {
+        return companyInformationService.updateAllCompanyInformation(dto.toServiceRequest(), logoImageUrl, sloganImageUrl);
+    }
+
+    @Operation(summary = "회사 로고 이미지 수정 API")
+    @PutMapping("/company/logo")
+    public ApiResponse updateCompanyLogoImage(@RequestPart(value = "logoImageUrl", required = false) MultipartFile logoImageUrl) {
+        return companyInformationService.updateCompanyLogoImage(logoImageUrl);
+    }
+
+    @Operation(summary = "회사 기본 정보(주소, 유선번호, 팩스번호) 수정 API")
+    @PutMapping("/company/basic")
+    public ApiResponse updateCompanyBasicInformation(@Valid @RequestBody UpdateCompanyBasicInformationRequestDto dto) {
+        return companyInformationService.updateCompanyBasicInformation(dto.toServiceRequest());
+    }
+
+    @Operation(summary = "회사 소개 정보(소개 문구, 로고 이미지 수정 API")
+    @PutMapping("/company/introduction")
+    public ApiResponse updateCompanyIntroductionInformation(@Valid @RequestPart("request") UpdateCompanyIntroductionInformationRequestDto dto,
+                                                            @RequestPart(value = "sloganImageUrl", required = false) MultipartFile sloganImageUrl) {
+        return companyInformationService.updateCompanyIntroductionInformation(dto.toServiceRequest(), sloganImageUrl);
+    }
+
+    @Operation(summary = "회사 5가지 상세 정보 수정 API")
+    @PutMapping("/company/detail")
+    public ApiResponse updateCompanyDetailInformation(@Valid @RequestBody UpdateCompanyDetailInformationRequestDto dto) {
+        return companyInformationService.updateCompanyDetailInformation(dto.toServiceRequest());
+    }
+
+    @Operation(summary = "회사 전체 정보 삭제 API")
+    @DeleteMapping("/company/information")
+    public ApiResponse deleteAllCompanyInformation() {
+        return companyInformationService.deleteAllCompanyInformation();
+    }
+
+    @Operation(summary = "회사 로고 이미지 삭제 API")
+    @DeleteMapping("/company/logo")
+    public ApiResponse deleteCampanyLogoImage() {
+        return companyInformationService.deleteCampanyLogoImage();
+    }
+
+    @Operation(summary = "회사 기본 정보(주소, 유선번호, 팩스번호) 삭제 API")
+    @DeleteMapping("/company/basic")
+    public ApiResponse deleteCompanyBasicInformation() {
+        return companyInformationService.deleteCompanyBasicInformation();
+    }
+
+    @Operation(summary = "회사 소개 정보 삭제 API")
+    @DeleteMapping("/company/introduction")
+    public ApiResponse deleteCompanyIntroductionInformation() {
+        return companyInformationService.deleteCompanyIntroductionInformation();
+    }
+
+    @Operation(summary = "회사 5가지 상세 정보 삭제 API")
+    @DeleteMapping("/company/detail")
+    public ApiResponse deleteCompanyDetailInformation() {
+        return companyInformationService.deleteCompanyDetailInformation();
     }
 }
