d5677c1773031e8a7e705f1cf6f765605537c53a
MoonInbae <mib010213@gmail.com>
2024-05-28 16:48:59 +0900

    [Feat] client 기능 구현
    - 공개여부 컬럼 Create 적용

--- src/main/java/com/example/promotionpage/domain/client/dto/request/CreateClientServiceRequestDto.java
+++ src/main/java/com/example/promotionpage/domain/client/dto/request/CreateClientServiceRequestDto.java
@@ -4,11 +4,13 @@ package com.example.promotionpage.domain.client.dto.request;
 import com.example.promotionpage.domain.client.domain.Client;
 
 public record CreateClientServiceRequestDto(
-    String name
+    String name,
+    Boolean visibility
 ) {
         public Client toEntity(String logoImg) {
             return Client.builder()
                     .name(name)
+                    .visibility(visibility)
                     .logoImg(logoImg)
                     .build();
         }
