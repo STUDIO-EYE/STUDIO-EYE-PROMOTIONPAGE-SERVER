91923bdf51698750d7abeb42ff9473711ea8f67b
MoonInbae <mib010213@gmail.com>
2024-06-03 18:10:59 +0900

    [Refactor] CEO Sonar 이슈 제거
    - Controller part
    - Service part

--- src/main/java/com/example/promotionpage/domain/ceo/api/CeoController.java
+++ src/main/java/com/example/promotionpage/domain/ceo/api/CeoController.java
@@ -1,11 +1,9 @@
 package com.example.promotionpage.domain.ceo.api;
 
 import com.example.promotionpage.domain.ceo.application.CeoService;
+import com.example.promotionpage.domain.ceo.domain.Ceo;
 import com.example.promotionpage.domain.ceo.dto.request.CreateCeoRequestDto;
 import com.example.promotionpage.domain.ceo.dto.request.UpdateCeoRequestDto;
-import com.example.promotionpage.domain.faq.domain.Faq;
-import com.example.promotionpage.domain.project.dto.request.CreateProjectRequestDto;
-import com.example.promotionpage.global.adapter.S3Adapter;
 import com.example.promotionpage.global.common.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -13,8 +11,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 @Tag(name = "CEO 정보 API", description = "CEO 정보 등록 / 수정 / 삭제 / 조회")
 @RestController
@@ -25,39 +21,39 @@ public class CeoController {
 
     @Operation(summary = "CEO 정보 등록 API")
     @PostMapping("/ceo")
-    public ApiResponse createCeoInformation(@Valid @RequestPart("request") CreateCeoRequestDto dto,
-                                     @RequestPart(value = "file", required = false) MultipartFile file){
+    public ApiResponse<Ceo> createCeoInformation(@Valid @RequestPart("request") CreateCeoRequestDto dto,
+                                                 @RequestPart(value = "file", required = false) MultipartFile file){
         return ceoService.createCeoInformation(dto.toServiceRequest(), file);
     }
 
     @Operation(summary = "CEO 전체 정보 조회 API")
     @GetMapping("/ceo")
-    public ApiResponse retrieveCeoInformation() {
+    public ApiResponse<Ceo> retrieveCeoInformation() {
         return ceoService.retrieveCeoInformation();
     }
 
     @Operation(summary = "CEO 전체 정보 수정 API")
     @PutMapping("/ceo")
-    public ApiResponse updateCeoInformation(@Valid @RequestPart("request") UpdateCeoRequestDto dto,
+    public ApiResponse<Ceo> updateCeoInformation(@Valid @RequestPart("request") UpdateCeoRequestDto dto,
                                             @RequestPart(value = "file", required = false) MultipartFile file) {
         return ceoService.updateCeoInformation(dto.toServiceRequest(), file);
     }
 
     @Operation(summary = "CEO 텍스트(이미지 제외) 정보 수정 API")
     @PutMapping("/ceo/modify")
-    public ApiResponse updateCeoTextInformation(@Valid @RequestPart("request") UpdateCeoRequestDto dto) {
+    public ApiResponse<Ceo> updateCeoTextInformation(@Valid @RequestPart("request") UpdateCeoRequestDto dto) {
         return ceoService.updateCeoTextInformation(dto.toServiceRequest());
     }
 
     @Operation(summary = "CEO 텍스트(이미지 제외) 정보 수정 API")
     @PutMapping("/ceo/image")
-    public ApiResponse updateCeoImageInformation(@RequestPart(value = "file", required = false) MultipartFile file) {
+    public ApiResponse<Ceo> updateCeoImageInformation(@RequestPart(value = "file", required = false) MultipartFile file) {
         return ceoService.updateCeoImageInformation(file);
     }
 
     @Operation(summary = "CEO 전체 정보 삭제 API")
     @DeleteMapping("/ceo")
-    public ApiResponse deleteCeoInformation() {
+    public ApiResponse<String> deleteCeoInformation() {
         return ceoService.deleteCeoInformation();
     }
 }
