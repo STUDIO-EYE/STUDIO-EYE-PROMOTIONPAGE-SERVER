becb31ab5cd3161da47f2da52502787e07ded333
diglowc <81141945+diglowc@users.noreply.github.com>
2024-06-05 20:35:49 +0900

    [Refactor] NoticeBoard 삭제
    사용되지 않는 NoticeBoard 삭제함.

--- src/main/java/com/example/promotionpage/domain/noticeBoard/dao/NoticeBoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.promotionpage.domain.noticeBoard.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.promotionpage.domain.noticeBoard.domain.NoticeBoard;
-
-public interface NoticeBoardRepository extends JpaRepository<NoticeBoard, Long> {
-}
