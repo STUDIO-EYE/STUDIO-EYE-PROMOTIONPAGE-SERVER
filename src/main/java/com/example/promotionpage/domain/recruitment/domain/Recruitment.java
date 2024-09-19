1f11151b5f27db720604c35969f7d29b9e316410
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-09-19 17:42:40 +0900

    [Feat] Recruitment Entity 생성

---
+++ src/main/java/com/example/promotionpage/domain/recruitment/domain/Recruitment.java
@@ -0,0 +1,30 @@
+package com.example.promotionpage.domain.recruitment.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Recruitment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @Builder
+    public Recruitment(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
