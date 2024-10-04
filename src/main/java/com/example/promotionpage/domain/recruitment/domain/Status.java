3b4e532caee81d1d177328838650f160dab8aa21
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-10-04 16:29:38 +0900

    [Fix] 채용공고 전체 조회 500에러 해결
    - 타입 에러(boolean)

--- src/main/java/com/example/promotionpage/domain/recruitment/domain/Status.java
+++ src/main/java/com/example/promotionpage/domain/recruitment/domain/Status.java
@@ -4,17 +4,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum Status {
-    PREPARING("시작전"),
-    OPEN("진행중"),
-    CLOSE("마감");
-
-    public final String label;
-
-//    private Status(String label) {
-//        this.label = label;
-//    }
-
-    public String getLabel() {
-        return this.label;
-    }
+    PREPARING,
+    OPEN,
+    CLOSE
 }
