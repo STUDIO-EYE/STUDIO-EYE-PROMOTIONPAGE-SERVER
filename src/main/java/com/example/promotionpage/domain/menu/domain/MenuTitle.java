9c4f8f53346354957053f8107a825451e8458c03
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-10-04 19:23:28 +0900

    [Feat] MenuTitle enum 파일 생성

---
+++ src/main/java/com/example/promotionpage/domain/menu/domain/MenuTitle.java
@@ -0,0 +1,14 @@
+package com.example.promotionpage.domain.menu.domain;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum MenuTitle {
+    MAIN,
+    ABOUT,
+    ARTWORK,
+    FAQ,
+    CONTACT,
+    NEWS,
+    RECRUITMENT
+}
