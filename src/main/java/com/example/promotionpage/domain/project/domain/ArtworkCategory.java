79d9f866e7364a3ce883008fc2e855afe58e8a87
MoonInbae <mib010213@gmail.com>
2024-10-09 22:49:14 +0900

    [Feat] ArtworkCategory enum 생성

---
+++ src/main/java/com/example/promotionpage/domain/project/domain/ArtworkCategory.java
@@ -0,0 +1,12 @@
+package com.example.promotionpage.domain.project.domain;
+
+public enum ArtworkCategory {
+    ENTERTAINMENT,
+    DRAMA,
+    DOCUMENTARY,
+    CHANNEL_OPERATING,
+    BRANDED,
+    MOTION_GRAPHIC,
+    ANIMATION,
+    LIVE_COMMERCE
+}
