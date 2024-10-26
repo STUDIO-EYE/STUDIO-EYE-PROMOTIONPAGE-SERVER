9fa741c936c7b22cd12e053096ae92635b291781
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-10-26 21:04:03 +0900

    [Feat] 회사 로고 조회 api 수정
    - repository에 쿼리 두 종류로 분기

--- src/main/java/com/example/promotionpage/domain/company_information/dao/CompanyInformationRepository.java
+++ src/main/java/com/example/promotionpage/domain/company_information/dao/CompanyInformationRepository.java
@@ -7,10 +7,15 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface CompanyInformationRepository extends JpaRepository<CompanyInformation, Long> {
-    @Query("SELECT c.logoImageUrl AS logoImageUrl FROM CompanyInformation c")
-    List<String> findLogoImageUrl();
+//    @Query("SELECT c.logoImageUrl AS logoImageUrl FROM CompanyInformation c")
+//    List<String> findLogoImageUrl();
+    @Query("SELECT c.lightLogoImageUrl AS lightLogoImageUrl FROM CompanyInformation c")
+    List<String> findLightLogoImageUrl();
+    @Query("SELECT c.darkLogoImageUrl AS darkLogoImageUrl FROM CompanyInformation c")
+    List<String> findDarkLogoImageUrl();
     @Query("SELECT c.address AS address, c.addressEnglish AS addressEnglish, c.phone AS phone, c.fax AS fax FROM CompanyInformation c")
     List<CompanyBasicInformation> findAddressAndPhoneAndFax();
     @Query("SELECT c.introduction AS introduction, c.sloganImageUrl AS sloganImageUrl FROM CompanyInformation c")
     List<CompanyIntroductionInformation> findIntroductionAndSloganImageUrl();
+
 }
